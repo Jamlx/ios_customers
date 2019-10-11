fbbc36e16334e416c8bd82dbb3be03bd5d066b69
James Cavanaugh <jamescav16@gmail.com>
2019-10-10 23:18:49 -0400

    auto constraints working

--- MyCustomerData/ViewController.swift
+++ MyCustomerData/ViewController.swift
@@ -13,8 +13,64 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("loaded")
+        print()
     }
+    
+    var firstName: String = ""
+    var lastName: String = ""
+    var address: String = ""
+    var address2: String = ""
+    var city: String = ""
+    var zip: Int = 0
+    
+    func clearValues(){
+        firstName = ""
+        lastName = ""
+        address = ""
+        address2 = ""
+        city = ""
+        zip = 0
+    }
+    
+    @IBAction func firstNameUpdate(_ sender: UITextField) {
+    }
+    
+    @IBAction func lastNameUpdate(_ sender: UITextField) {
+    }
+    
+    @IBAction func addressUpdate(_ sender: UITextField) {
+    }
+    
+    @IBAction func address2Update(_ sender: UITextField) {
+    }
+    
+    @IBAction func cityUpdate(_ sender: UITextField) {
+    }
+    
+    @IBAction func stateUpdate(_ sender: UITextField) {
+    }
+    
+    @IBAction func zipUpdate(_ sender: UITextField) {
+    }
+    
 
+    
+    @IBAction func btnAddClick(_ sender: UIButton) {
+        
+    }
+    
+
+    @IBAction func btnRemoveAllClick(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnRemoveLastClick(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func btnPrintListClick(_ sender: UIButton) {
+    }
+    
 
 }
 
